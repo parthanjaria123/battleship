0c98443e374e3e5fd464bd15334bc017eb335f69
Ayush Dewan <ayushdewan02@gmail.com>
2019-12-12 08:18:06 -0800

    added second grid

--- main.cpp
+++ main.cpp
@@ -4,30 +4,42 @@
 using namespace sf;
 using namespace std;
 
-RenderWindow window(VideoMode(1000, 1000), "Battleship");
-RectangleShape grid[10][10];
+const int grid_width = 500, margin = 100;
+const int window_width = 2 * grid_width + 3 * margin, window_height = grid_width + 2 * margin;
 
+RenderWindow window(VideoMode(window_width, window_height), "Battleship");
+RectangleShape grid[10][10];
 
 void show_battleship(/*int x_coordinate, int y_coordinate*/)
 {
     ConvexShape convex;
     convex.setPointCount(5);
-    convex.setPoint(0,Vector2f(200.f,100.f));
-    convex.setPoint(1,Vector2f(300.f,100.f));
-    convex.setPoint(2,Vector2f(300.f,300.f));
-    convex.setPoint(3,Vector2f(250.f,400.f));
-    convex.setPoint(4,Vector2f(200.f,300.f));
+    convex.setPoint(0,Vector2f(100.f,100.f));
+    convex.setPoint(1,Vector2f(400.f,100.f));
+    convex.setPoint(2,Vector2f(400.f,700.f));
+    convex.setPoint(3,Vector2f(250.f,850.f));
+    convex.setPoint(4,Vector2f(100.f,700.f));
 
-    convex.setFillColor(Color::Red);
+    convex.setFillColor(Color::White);
 
-        Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-                window.close();
+    window.draw(convex);
+}
+
+void draw_gridlines(float x, float y)
+{
+    Vector2f cellSize(50.f, 50.f);
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            grid[i][j].setSize(cellSize);
+            grid[i][j].setOutlineColor(Color::Black);
+            grid[i][j].setOutlineThickness(5.0f);
+
+            grid[i][j].setPosition(i*cellSize.x + 1.0f + x, j*cellSize.y + 1.0f + y);
+
+            window.draw(grid[i][j]);
         }
-
-        window.draw(convex);
+    }
 }
 
 void show_grid()
@@ -41,23 +53,15 @@ void show_grid()
                 window.close();
         }
 
-        Vector2f cellSize(100.f, 100.f);
 
-        for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
-                grid[i][j].setSize(cellSize);
-                grid[i][j].setOutlineColor(Color::Black);
-                grid[i][j].setOutlineThickness(5.0f);
 
-                grid[i][j].setPosition(i*cellSize.x + 1.0f, j*cellSize.y + 1.0f);
+        draw_gridlines(margin, margin);
+        draw_gridlines(grid_width + 2 * margin, margin);
 
-                window.draw(grid[i][j]);
+        //show_battleship();
 
-            }
-        }
-
-        show_battleship();
         window.display();
+        window.clear(Color::Cyan);
     }
 
 }
